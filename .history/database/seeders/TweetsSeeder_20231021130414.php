0dc40f90193d0c87e59a6250456607f7ecd9c577
masamichan <positivemasa031@gmail.com>
2023-10-21 14:27:25 +0900

    first commit

---
+++ .history/database/seeders/TweetsSeeder_20231021130414.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TweetsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
